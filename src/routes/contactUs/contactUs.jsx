6704c2e36bbb92119525e6228a44aa865850388a
raysonoon <raysonoonhere@gmail.com>
2025-03-05 11:55:12 +0800

    Added headerReach and bodyText and adjusted alignment

--- src/routes/contactUs/contactUs.jsx
+++ src/routes/contactUs/contactUs.jsx
@@ -4,27 +4,35 @@ import styles from "./ContactUs.module.css";
 
 const ContactUs = () => {
     return (
-        <section className={styles.contact}>
-            <form>
-                <div className={styles.title}>
-                    <Typography variant="smallHeading">Contact Form</Typography>
-                    <Typography variant="subtitle">Fill up the form below to send us a message</Typography>
+        <div className={styles.contentWrapper}>
+            <section className={styles.contact}>
+                <div className={styles.headerReach}>
+                    <Typography variant="heading">REEEACH OUT!</Typography>
                 </div>
-                <div className={styles.inputBox}>
-                    <label>Full Name</label>
-                    <input type="text" className={styles.field} placeholder="Enter your name" required />
+                <div className={styles.bodyText}>
+                    <Typography variant="body">Have some curious questions, inquiries or feedback about Garage@EEE? Contact us here or through our email</Typography>
                 </div>
-                <div className={styles.inputBox}>
-                    <label>Email</label>
-                    <input type="email" className={styles.field} placeholder="Enter your email" required />
-                </div>
-                <div className={styles.inputBox}>
-                    <label>Your Message</label>
-                    <textarea name="" id="" className={`${styles.field} ${styles.mess}`} placeholder="Enter your message" required></textarea>
-                </div>
-                <button type="submit">Send Message</button>
-            </form>
-        </section>
+                <form>
+                    <div className={styles.formTitle}>
+                        <Typography variant="smallHeading">Contact Form</Typography>
+                        <Typography variant="subtitle">Fill up the form below to send us a message</Typography>
+                    </div>
+                    <div className={styles.inputBox}>
+                        <label>Full Name</label>
+                        <input type="text" className={styles.field} placeholder="Enter your name" required />
+                    </div>
+                    <div className={styles.inputBox}>
+                        <label>Email</label>
+                        <input type="email" className={styles.field} placeholder="Enter your email" required />
+                    </div>
+                    <div className={styles.inputBox}>
+                        <label>Your Message</label>
+                        <textarea name="" id="" className={`${styles.field} ${styles.mess}`} placeholder="Enter your message" required></textarea>
+                    </div>
+                    <button type="submit">Send Message</button>
+                </form>
+            </section>
+        </div>
     );
 }
 
