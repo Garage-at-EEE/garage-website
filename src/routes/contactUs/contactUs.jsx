2d2eaa0ae4e9e093a899417097fbe142bd9394b0
raysonoon <raysonoonhere@gmail.com>
2025-03-05 11:05:52 +0800

    Added contact form subtitle and adjust typography style

--- src/routes/contactUs/contactUs.jsx
+++ src/routes/contactUs/contactUs.jsx
@@ -1,10 +1,15 @@
+import Typography from "../../components/typography/Typography";
+
 import styles from "./ContactUs.module.css";
 
 const ContactUs = () => {
     return (
         <section className={styles.contact}>
             <form>
-                <h2>Contact Form</h2>
+                <div className={styles.title}>
+                    <Typography variant="smallHeading">Contact Form</Typography>
+                    <Typography variant="subtitle">Fill up the form below to send us a message</Typography>
+                </div>
                 <div className={styles.inputBox}>
                     <label>Full Name</label>
                     <input type="text" className={styles.field} placeholder="Enter your name" required />
