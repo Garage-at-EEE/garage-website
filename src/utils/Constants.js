06fbe8984c1ba9dd9d8c91966f0fa2d6458a2ae3
Avy <gibraltar.av@gmail.com>
2025-03-31 22:37:28 +0800

    Update API to final deployment for assignedproject

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,6 @@
 // Update this when using new AppScript Deployment
 export const API_DOMAIN =
-"https://script.google.com/macros/s/AKfycbz8qMV8LWqH4z7gRCo3cYEqUx3Ns-rmUSDbvNbzeR1WOGwDaLh0NdjUBUtzfUKvQs27uA/exec";
+"https://script.google.com/macros/s/AKfycbyZK1p-MmsGj6OaHFcnHUh9C2FXNe4G6nGoACBOiMwfyUj9kfrw8KMQmXk3rKXS2WnLNg/exec";
 // Do not update LOGIN_DOMAIN! Used for login POST method
 export const LOGIN_DOMAIN =
 "https://script.google.com/macros/s/AKfycbxooNFuPLMU2fSji-2U4THouVRdbWEJuT0e8Zs0JRMUh50TWveQdS4frPUEYcoeQhkDvg/exec";
