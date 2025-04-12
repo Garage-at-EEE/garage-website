aac845795e09143a3ef6364a7959d25977e02d21
Avy <gibraltar.av@gmail.com>
2025-04-12 17:02:28 +0800

    Update layout of AssignedProjectDetail
    - Add 3 sections to AssignedProjectDetail:
        - Project Description
        - Project Recruitment Info
        - Project Registration Link
    - Sections in AssignedProjectDetail based on data.isRecruiting

--- src/routes/assignedProjects/AssignedProjectDetail.js
+++ src/routes/assignedProjects/AssignedProjectDetail.js
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom";
-import Carousel from "../../components/carousel/Carousel";
 import Typography from "../../components/typography/Typography";
 import useFetch from "../../hooks/useFetch";
 import { API_DOMAIN } from "../../utils/Constants";
@@ -8,12 +7,10 @@ import PageTemplate from "../../components/pageTemplate/PageTemplate";
 import PageGap from "../../components/pageGap/PageGap";
 import HeroImage from "../../components/heroImage/heroImage";
 
-import styles from "./AssignedProjectDetail.module.css";
-
 function ProjectDetail() {
   const params = useParams();
   const id = params.id;
-  const { data, isLoading } = useFetch({ // TODO: change url get to assigned projects data
+  const { data, isLoading } = useFetch({ 
     url: API_DOMAIN + "?type=assignedProjectInfo&index=" + id,
   });
   return (
@@ -26,11 +23,28 @@ function ProjectDetail() {
               src={data.coverPic}
               subheading={data.tagline}
             />
-            <Typography variant="body">{data.description}</Typography>
-            {/* <div className={styles["gallery-box"]}>
-              <Typography variant="smallHeading">Gallery</Typography>
-              {data.photos && <Carousel images={data.photos} />}
-            </div> */}
+            <div>
+              <Typography variant="smallHeading">DESCRIPTION</Typography>
+              <Typography variant="body">{data.description}</Typography>
+            </div>
+            {(data.isRecruiting === "Y") ?
+            <>
+              <div>
+                <Typography variant="smallHeading">RECRUITMENT</Typography>
+                <Typography variant="body">{data.recruitment}</Typography>
+              </div>
+              <div>
+                <Typography variant="smallHeading">REGISTRATION LINK</Typography>
+                <Typography variant="smallHeading">
+                <a href={data.registerLink} target="_blank" rel="noopener noreferrer">{data.registerLink}</a>
+                </Typography>
+              </div>
+            </>
+            :
+            <Typography variant="smallHeading">
+            Sorry, this project is not currently recruiting.
+            </Typography>
+            }
           </PageGap>
         )}
       </PageTemplate>
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,6 @@
 // Update this when using new AppScript Deployment
 export const API_DOMAIN =
-"https://script.google.com/macros/s/AKfycbyZK1p-MmsGj6OaHFcnHUh9C2FXNe4G6nGoACBOiMwfyUj9kfrw8KMQmXk3rKXS2WnLNg/exec";
+"https://script.google.com/macros/s/AKfycbwlLa8Vm8hn6eqIatqyn82e-A1WgXwArL62iUtmaidlHdsSdTHpIxIqVkCeY0ODOjKvrQ/exec";
 // Do not update LOGIN_DOMAIN! Used for login POST method
 export const LOGIN_DOMAIN =
 "https://script.google.com/macros/s/AKfycbxooNFuPLMU2fSji-2U4THouVRdbWEJuT0e8Zs0JRMUh50TWveQdS4frPUEYcoeQhkDvg/exec";
