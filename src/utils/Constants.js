c4a314bf772e3ea1cd7e9a1c9b26cd3a35c18bea
totoro-7 <yinyu18.tan@gmail.com>
2025-05-02 01:09:09 +0800

    Update with appscript
    Data integrated with frontend from duplicated appscript and google sheet

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,2 +1,5 @@
 export const API_DOMAIN =
+  "https://script.google.com/macros/s/AKfycbxwt64YkHN8DQu9iIW7QzhXSYoxJ5gSDn1qk8vsD4oZgiQrQJzHwSygFutrx65_pTHtGg/exec";
+
+  export const LOGIN_DOMAIN =
   "https://script.google.com/macros/s/AKfycbwpBDp51U6tRBM9MC_3-QYAOSsEoyoYtcwWOWxXBwy8FhcEy7FcltekRybuZOfmfv3EgQ/exec";
