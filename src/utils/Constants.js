e22f7f747fb739c6e3b8299503f8fc155bddf413
Avy <gibraltar.av@gmail.com>
2025-09-01 22:40:47 +0800

    Hot fix (1/9/2025)

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,6 @@
 // Update this when using new AppScript Deployment (now: API 1.0.6)
 export const API_DOMAIN =
-"https://script.google.com/macros/s/AKfycbznTb8pLQ0ftXTgWjxYGGLxNcuOUv-mWVjgwfTwTT1vealw8JzXOjMnlzqppPtm0uiZAA/exec";
+"https://script.google.com/macros/s/AKfycbxYJlf2ZSuNApWCfk8CxcSC8gl9p-iHn1EI8vmz3ytIuQuJog2xgAsqTTo7-Q-8fdu-uQ/exec";
 // Do not update LOGIN_DOMAIN! Used for login POST method
 export const LOGIN_DOMAIN =
 "https://script.google.com/macros/s/AKfycbxooNFuPLMU2fSji-2U4THouVRdbWEJuT0e8Zs0JRMUh50TWveQdS4frPUEYcoeQhkDvg/exec";
