46e038ca0adb67f68bd60d76a31058b898e680f1
menujaam <mmenujaa888@gmail.com>
2026-01-01 01:16:50 +0800

    If retrieve a blank in column, dont show the content

--- src/routes/projects/ProjectDetail.js
+++ src/routes/projects/ProjectDetail.js
@@ -27,28 +27,35 @@ function ProjectDetail() {
               src={data.coverPic}
               subheading={data.tagline}
             />
+
             <Typography variant="body">{data.description}</Typography>
 
+            {data.resourcesUsed && data.resourcesUsed.trim() !== "" && (
             <div className={styles["content-box"]}>
               <Typography variant="smallHeading">Resources Used</Typography>
               <Typography variant="body">{data.resourcesUsed}</Typography>
             </div>
-  
+            )}
+            
+            
             <div className={styles["gallery-box"]}>
               <Typography variant="smallHeading">Gallery</Typography>
               {data.photos && <Carousel images={data.photos} />}
             </div>
+            
 
+            {data.reflection && data.reflection.trim() !== "" && (
             <div className={styles["content-box"]}>
               <Typography variant="smallHeading">Reflection</Typography>
               <Typography variant="body">{data.reflection}</Typography>
             </div>
+            )}
 
             <div className={styles["content-box"]}>
               <Typography variant="smallHeading">Contact Us On Telegram</Typography>
               <Typography variant="body">{data.contactInfo}</Typography>
             </div>
-            
+  
           </PageGap>
         )}
       </PageTemplate>
